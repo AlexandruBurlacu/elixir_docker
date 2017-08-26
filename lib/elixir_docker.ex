ec9f08905bed34e9853a6e8ce97b9a26942a2bdc
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-08-26 09:20:50 +0300

    first commit

---
+++ lib/elixir_docker.ex
@@ -0,0 +1,18 @@
+defmodule ElixirDocker do
+  @moduledoc """
+  Documentation for ElixirDocker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirDocker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
