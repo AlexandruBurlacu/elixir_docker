412a823c109a340dddaf691c01e19e113d018a74
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-02 19:51:14 +0300

    pass existent tests

--- test/users_test.exs
+++ test/users_test.exs
@@ -2,8 +2,13 @@ defmodule UserTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  test "count users" do
+  test "is map" do
     assert is_map Users.count("http://jsonplaceholder.typicode.com/users")
+  end
+
+  test "count users" do
+    assert [:count , :inserted_at, :updated_at, :id, :__meta__]
+    |> Enum.all?(&(Map.has_key?(Users.count("http://jsonplaceholder.typicode.com/users"), &1)))
   end
 
   test "wrong url" do
