c007f0e0ee3f184bdc2d856f8867c79df5544b86
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-15 12:30:20 +0300

    Works with docker-compose up after loading data into DB via docker-compose run web iex

--- lib/router.ex
+++ lib/router.ex
@@ -6,6 +6,9 @@ defmodule ElixirDocker.Router do
   alias ElixirDocker.Repo
   alias Plug.Adapters.Cowboy
 
+  require Logger
+  require Poison
+
   plug :match
   plug :dispatch
 
@@ -20,20 +23,24 @@ defmodule ElixirDocker.Router do
   get "/status" do
     if System.get_env("MIX_ENV") == "test" do
       timestamp = Ecto.DateTime.from_date_and_time(%Ecto.Date{year: 2017, month: 09, day: 03},
-                                                   %Ecto.Time{hour: 12, min: 42, sec: 50, usec: 00})
+                                                 %Ecto.Time{hour: 12, min: 42, sec: 50, usec: 00})
       last_run = %{inserted_at: timestamp, count: 10}
     else
       last_run = Record |> Ecto.Query.last |> Repo.one
     end
 
+    data = Poison.encode!(%{last_run: last_run.inserted_at,
+                            result_count: last_run.count})
+
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(%{last_run: last_run.inserted_at,
-                                       result_count: last_run.count}))
+    |> (fn (x) -> Logger.error "It's ok"; x end).()
+    |> send_resp(200, data)
   end
 
   match _ do
     conn
+    |> (fn (x) -> Logger.error "It's ok"; x end).()
     |> send_resp(404, Poison.encode!(%{endpoint: "not implemented!"}))
   end
 end
