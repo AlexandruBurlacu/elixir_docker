c16eaac74f8c209a9a67834665762b22177ddec0
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-23 21:44:09 +0300

    modify the database schema

--- lib/record.ex
+++ lib/record.ex
@@ -2,8 +2,11 @@ defmodule ElixirDocker.Record do
   use Ecto.Schema
 
   schema "records" do
-    field :count, :integer
+    field :title, :string
+    field :description, :string
+    field :due_date, Ecto.Date
+    field :priority, :string
 
-    timestamps
+    timestamps()
   end
 end
