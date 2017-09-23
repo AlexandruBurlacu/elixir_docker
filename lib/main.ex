1fcdab5532e5092eb79350d00afceb6938e1a1ab
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-23 21:51:19 +0300

    remove users.ex file

---
+++ lib/main.ex
@@ -0,0 +1,22 @@
+defmodule ElixirDocker do
+  @moduledoc """
+  Documentation for ElixirDocker.
+  """
+  
+  use Application
+  alias Users
+  require Logger
+  import Supervisor.Spec  
+
+  def start(_type, _args) do
+    children = [
+      worker(ElixirDocker.Repo, []),
+      worker(ElixirDocker.Router, [])
+    ]
+
+    Logger.info "Started application"
+
+    Supervisor.start_link(children, strategy: :one_for_one,
+                          name: ElixirDocker.Supervisor)
+  end
+end
