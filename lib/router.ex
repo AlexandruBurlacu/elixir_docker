4ae913e1a7cc4c598bbca8cc39e182b8cbc0824c
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-23 21:50:59 +0300

    define REST endpoints

--- lib/router.ex
+++ lib/router.ex
@@ -17,25 +17,39 @@ defmodule ElixirDocker.Router do
     {:ok, _} = Cowboy.http(ElixirDocker.Router, [], port: 8080)
   end
 
-  get "/status" do
-    if System.get_env("MIX_ENV") == "test" do
-      timestamp = Ecto.DateTime.from_date_and_time(%Ecto.Date{year: 2017, month: 09, day: 03},
-                                                 %Ecto.Time{hour: 12, min: 42, sec: 50, usec: 00})
-      last_run = %{inserted_at: timestamp, count: 10}
-    else
-      last_run = Record |> Ecto.Query.last |> Repo.one
-    end
-
-    data = Poison.encode!(%{last_run: last_run.inserted_at,
-                            result_count: last_run.count})
-
+  get "/v1/tasks" do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, data)
+    |> send_resp(200, Poison.encode!(%{data: "Nothing to show"}))
+  end
+
+  get "/v1/tasks/{id}" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{data: "Nothing to show"}))
+  end
+
+  post "/v1/tasks" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{data: "Nothing to show"}))
+  end
+
+  put "/v1/tasks/{id}" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{data: "Nothing to show"}))
+  end
+
+  delete "/v1/tasks/{id}" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{data: "Nothing to show"}))
   end
 
   match _ do
     conn
+    |> put_resp_content_type("application/json")
     |> send_resp(404, Poison.encode!(%{endpoint: "not implemented!"}))
   end
 end
