d02c6ecb670c9391e708038f4ca828a88ab2a97e
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-24 15:09:53 +0300

    implement POST endpoint

--- lib/task_router.ex
+++ lib/task_router.ex
@@ -5,7 +5,9 @@ defmodule ElixirDocker.Router.TasksRouter do
   """
 
   use Plug.Router
-  require Logger
+
+  alias ElixirDocker.Record
+  alias ElixirDocker.Repo
 
   plug :match
   plug Plug.Parsers, parsers: [:json],
@@ -13,39 +15,60 @@ defmodule ElixirDocker.Router.TasksRouter do
                      json_decoder: Poison
   plug :dispatch
 
-  defp common_behaviour(connection, data) do
+  defp response(connection, code, data) do
     connection
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(data))
+    |> send_resp(code, Poison.encode!(data))
+  end
+
+  defp valid_data?(data) do
+    ["title", "description", "due_date", "priority"]
+    |> Enum.map(fn k ->
+      data
+      |> Map.has_key?(k)
+    end)
+    |> Enum.all?
   end
 
   post "/" do
-    conn
-    |> common_behaviour(%{info: "Welcome from post",
-                          data: conn.body_params})
+    data = conn.body_params
+
+    if (valid_data?(data)) do
+
+      record = %Record{title: data["title"],
+                       description: data["description"],
+                       due_date: Date.from_iso8601!(data["due_date"]),
+                       priority: data["priority"]}
+
+      resp = Repo.insert! record
+
+      conn |> response(201, %{info: "Task created", task_id: resp.id})
+    else
+      conn |> response(400, %{info: "Invalid body"})
+    end
   end
 
   get "/" do
     conn
-    |> common_behaviour(%{info: "Welcome from listing",
+    |> response(200, %{info: "Welcome from listing",
                           data: [%{no: "posts"}, %{no: "posts"}]})
   end
 
   get "/:id" do
     conn
-    |> common_behaviour(%{info: "Welcome from details",
+    |> response(200, %{info: "Welcome from details",
                           data: %{no: "info on #{id}"}})
   end
 
   put "/:id" do
     conn
-    |> common_behaviour(%{info: "Welcome from update",
+    |> response(200, %{info: "Welcome from update",
                           data: %{no: "way to update #{id}"}})
   end
 
   delete "/:id" do
     conn
-    |> common_behaviour(%{info: "Welcome from delete",
+    |> response(200, %{info: "Welcome from delete",
                           data: %{no: "way to delete #{id}"}})
   end
 end
