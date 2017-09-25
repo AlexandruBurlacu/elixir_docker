dba3231f1f0fa0b2dd0d7b62eceb5e20ad6ea79f
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-09-25 13:14:03 +0300

    add tests

--- test/test_router.exs
+++ test/test_router.exs
@@ -4,28 +4,52 @@ defmodule RouterTest do
   use Plug.Test
   
   @opts ElixirDocker.Router.init([])
-  
+
   property :returns_404 do
     for_all _ in int() do
-      conn = conn(:get, "/")
-  
-      conn = ElixirDocker.Router.call(conn, @opts)
-  
-      assert conn.state == :sent
-      assert conn.status == 404
-      assert conn.resp_body == Poison.encode!(%{endpoint: "not implemented!"})
-    end
-  end
-
-  property :returns_ok do
-    for_all _ in int() do
-      conn = conn(:get, "/status")
+      conn = conn(:get, "/whatever")
     
       conn = ElixirDocker.Router.call(conn, @opts)
 
       assert conn.state == :sent
-      assert conn.status == 200
-      assert conn.resp_body =~ ~r/{"result_count":[0-9]*,"last_run":"[0-9-]*T[0-9:]*"}/i
+      assert conn.status == 404
+      assert conn.resp_body =~ ~r/{"endpoint":"not implemented!"}/i
+    end
+  end
+
+  property :post_id_returns_404 do
+    for_all id in int() do
+      conn = conn(:post, "/v1/tasks/#{id}")
+  
+      conn = ElixirDocker.Router.call(conn, @opts)
+  
+      assert conn.state == :sent
+      assert conn.status == 405
+      assert conn.resp_body =~ ~r/{"info":"Impossible to update tasks via POST, use PUT instead"/i
+    end
+  end
+
+  property :put_root_returns_404 do
+    for_all _ in int() do
+      conn = conn(:put, "/v1/tasks/")
+  
+      conn = ElixirDocker.Router.call(conn, @opts)
+  
+      assert conn.state == :sent
+      assert conn.status == 405
+      assert conn.resp_body =~ ~r/{"info":"Impossible to update all tasks"/i
+    end
+  end
+
+  property :delete_root_returns_404 do
+    for_all _ in int() do
+      conn = conn(:delete, "/v1/tasks/")
+  
+      conn = ElixirDocker.Router.call(conn, @opts)
+  
+      assert conn.state == :sent
+      assert conn.status == 405
+      assert conn.resp_body =~ ~r/{"info":"Impossible to delete all tasks"/i
     end
   end
 end
